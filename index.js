e37f99f923f6bae35dea34fd4d8b904572a0d850
manikandan <manikandansitpl3@gmail.com>
2023-05-28 00:22:03 +0530

    host

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
 
-const url = 'mongodb://127.0.0.1:27017/peoples'; 
+const url = 'mongodb://localhost:27017/peoples'; 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB!');
