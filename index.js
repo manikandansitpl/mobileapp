f1d788aa8333b60a47ddaa2f898911ddf5ad8455
manikandan <manikandansitpl3@gmail.com>
2023-05-29 20:12:47 +0530

    db change

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const iam = require('./awsIam')
 var morgan = require('morgan')
 var mongoose = require('mongoose')
+var MongoClient = require('mongodb').MongoClient;
 const router = require('./router');
 const cors = require('cors')
 //middle ware
@@ -14,12 +15,21 @@ app.use(cors())
 const url = 'mongodb+srv://manikandansitpl3:83mb1dmaopS%40@mobilecluster0.nkmpjqj.mongodb.net/';
 // const connectionString = 'mongodb://0.0.0.0:27017/';
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB!');
-    // Perform the data deployment here
-  })
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB!');
+//     // Perform the data deployment here
+//   })
+//   .catch(err => console.error('Error connecting to MongoDB:', err));
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection("customers", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
 
 app.use('/api/person', router)
 // Call the listUsers function
