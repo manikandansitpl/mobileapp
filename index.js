dd0129c57171f00cf09b7599aef95db3b51b7ea0
manikandan <manikandansitpl3@gmail.com>
2023-05-29 20:13:56 +0530

    db

--- index.js
+++ index.js
@@ -15,21 +15,12 @@ app.use(cors())
 const url = 'mongodb+srv://manikandansitpl3:83mb1dmaopS%40@mobilecluster0.nkmpjqj.mongodb.net/';
 // const connectionString = 'mongodb://0.0.0.0:27017/';
 
-// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB!');
-//     // Perform the data deployment here
-//   })
-//   .catch(err => console.error('Error connecting to MongoDB:', err));
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  dbo.createCollection("customers", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
-});
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+    // Perform the data deployment here
+  })
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.use('/api/person', router)
 // Call the listUsers function
